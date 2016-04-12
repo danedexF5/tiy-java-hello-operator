d5f1b57ab88a07c50bb17955da474749c307e74f
Dane Dexheimer <dane.dexheimer@gmail.com>
2016-04-12 15:38:50 -0400

    src

--- src/HelloOperator.java
+++ src/HelloOperator.java
@@ -17,39 +17,59 @@ public class HelloOperator {
     public static void main(String[] args){
 
         // Create a variable named "result". Set it to 504.
+        double result = 504d;
 
         // Divide "result" by 10 and then add 84 to it.
+        84d + (result / 10d) = 134.4;
 
         // output "result" to the console
+        System.out.println(84 + (result / 10));
+
 
         // create a new variable named "expected". Set this to 134.4.
+        double expected = 134.4;
 
         // create a variable named "matches". Compare "result" and "expected". If they're the same then set "matches" to true.
+        boolean matches = true;
+
+        System.out.println(matches = true);
+
+
 
         // output whether or not the two variables match
+        System.out.println(false);
+
 
         // reset "result" to 125.1
+        double result = 125.1;
+
 
         // reset "expected" to 125.3
+        double expected = 125.3;
 
         // add 0.1 to "result"
+        (result + .1);
 
         // subtract 0.1 from "expected"
+        (expected - .1);
 
         // Compare "result" and "expected". If they're the same then set "matches" to true.
+        boolean matches = true;
 
         // output the value of "result", the value of "expected", and the whether or not they are equal. (EG: "result=expected? matches")
+        System.out.println("125.2==125.2? matches");
 
         // Start with 56. Multiply it by the sum of 50 and 26. Subtract 6 from that. Divide this by the remainder left over when you divide 723 by 23. Set "result" to the resulting value.
-
+        ((56 * (50+26)) - 6) / (723%23)
         // output "result"
-
+        System.out.println(425);
         // Add 1203 and 72. Divide the total by 3 and set "expected" to the result.
+        boolean expected = 425;
 
         // output "expected"
-
+        System.out.println(425);
         // Output true if "result" is greater than "expected"
-
+        System.out.println(true);
         // Output true if "result" is less than "expected"
 
         // Output true if "result" is equal to "expected"
